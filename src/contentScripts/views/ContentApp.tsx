e56a17ec36f49141c8b6c23e8c84a1f1cc2a19cc
Nguyen Minh Tuan <tnowad@gmail.com>
2023-07-15 11:57:30 +0700

    feat(content): add button

--- src/contentScripts/views/ContentApp.tsx
+++ src/contentScripts/views/ContentApp.tsx
@@ -1,3 +1,9 @@
 export const ContentApp = () => {
-  return <div>Hey!</div>;
+  return (
+    <div className="fixed right-0 bottom-0 m-5 z-100 flex items-end font-sans select-none leading-1em">
+      <button className="flex w-10 h-10 rounded-full shadow cursor-pointer border-none">
+        icon
+      </button>
+    </div>
+  );
 };
