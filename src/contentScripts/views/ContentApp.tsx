9c761ad440e56a60454b0c15f0c5b43843b9c63f
Nguyen Minh Tuan <tnowad@gmail.com>
2023-07-15 12:15:21 +0700

    feat(content): add event button

--- src/contentScripts/views/ContentApp.tsx
+++ src/contentScripts/views/ContentApp.tsx
@@ -1,11 +1,31 @@
-import "~/styles";
 import { DocumentArrowDownIcon } from "@heroicons/react/24/solid";
+import { fillColor, parseSubject } from "~/utils/parseData";
+import Subject from "~/interfaces/Subject";
+
 export const ContentApp = () => {
+  const getData = (): Subject[] => {
+    const trElements = Array.from(document.querySelectorAll(".body-table tr"));
+    const data = trElements.map((trElement) =>
+      parseSubject(Array.from(trElement.querySelectorAll("td")))
+    );
+
+    return fillColor(data);
+  };
+
+  const handleButtonClick = () => {
+    console.log(getData());
+  };
+
   return (
-    <div className="fixed right-0 bottom-0 m-5 z-100 flex items-end font-sans select-none leading-1em">
-      <button className="flex w-10 h-10 rounded-full shadow cursor-pointer border-none bg-blue-400 hover:bg-blue-500">
-        <DocumentArrowDownIcon className="text-white" />
-      </button>
+    <div>
+      <div className="fixed right-0 bottom-0 m-5 z-100 flex items-end font-sans select-none leading-1em">
+        <button
+          className="flex w-10 h-10 rounded-full shadow cursor-pointer border-none bg-blue-400 hover:bg-blue-500"
+          onClick={handleButtonClick}
+        >
+          <DocumentArrowDownIcon className="text-white" />
+        </button>
+      </div>
     </div>
   );
 };
