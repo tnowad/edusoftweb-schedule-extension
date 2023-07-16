b5e31c7298b7d531a2b1de732fa8f5391af18d36
Nguyen Minh Tuan <tnowad@gmail.com>
2023-07-17 00:38:58 +0700

    refactor(project): format code style

--- src/utils/parseData.ts
+++ src/utils/parseData.ts
@@ -1,4 +1,4 @@
-import Subject from "~/interfaces/Subject";
+import Subject from '~/interfaces/Subject'
 
 export const parseDayOfWeek = (day: string): number => {
   const daysOfWeek: Record<string, number> = {
@@ -13,26 +13,24 @@ export const parseDayOfWeek = (day: string): number => {
     Wed: 4,
     Thu: 5,
     Fri: 6,
-    Sat: 7,
-  };
-  return daysOfWeek[day];
-};
+    Sat: 7
+  }
+  return daysOfWeek[day]
+}
 
 export const parseWeeks = (weeksText: string): number[] => {
   // convert to time in number
   return weeksText
-    .split("--")
-    .map((week) =>
-      new Date(week.replace(/(\d+[/])(\d+[/])/, "$2$1")).getTime(),
-    );
-};
+    .split('--')
+    .map((week) => new Date(week.replace(/(\d+[/])(\d+[/])/, '$2$1')).getTime())
+}
 
 export const parseSubject = (tdElements: Element[]): Subject => {
   const subjectText = tdElements.reduce(
     (previous, current) => `${previous + (current as HTMLElement).innerText}|`,
-    "",
-  );
-  const subjectFields = subjectText.split("|");
+    ''
+  )
+  const subjectFields = subjectText.split('|')
 
   return {
     subjectCode: subjectFields[0],
@@ -48,45 +46,45 @@ export const parseSubject = (tdElements: Element[]): Subject => {
     numberOfPeriods: parseInt(subjectFields[10]),
     room: subjectFields[11],
     lecturer: subjectFields[12],
-    weeks: parseWeeks(tdElements[13].innerHTML.split("'")[1]),
-  };
-};
+    weeks: parseWeeks(tdElements[13].innerHTML.split("'")[1])
+  }
+}
 
 export const parsePeriodTime = (
-  period: number,
+  period: number
 ): { start: string; end: string } => {
   const periodTime: Record<number, { start: string; end: string }> = {
-    1: { start: "07:00", end: "07:50" },
-    2: { start: "07:50", end: "08:40" },
-    3: { start: "09:00", end: "09:50" },
-    4: { start: "09:50", end: "10:40" },
-    5: { start: "10:40", end: "11:30" },
-    6: { start: "13:00", end: "13:50" },
-    7: { start: "13:50", end: "14:40" },
-    8: { start: "15:00", end: "15:50" },
-    9: { start: "15:50", end: "16:40" },
-    10: { start: "16:40", end: "17:30" },
-    11: { start: "17:40", end: "18:30" },
-    12: { start: "18:30", end: "19:20" },
-    13: { start: "19:20", end: "20:10" },
-  };
-  return periodTime[period];
-};
+    1: { start: '07:00', end: '07:50' },
+    2: { start: '07:50', end: '08:40' },
+    3: { start: '09:00', end: '09:50' },
+    4: { start: '09:50', end: '10:40' },
+    5: { start: '10:40', end: '11:30' },
+    6: { start: '13:00', end: '13:50' },
+    7: { start: '13:50', end: '14:40' },
+    8: { start: '15:00', end: '15:50' },
+    9: { start: '15:50', end: '16:40' },
+    10: { start: '16:40', end: '17:30' },
+    11: { start: '17:40', end: '18:30' },
+    12: { start: '18:30', end: '19:20' },
+    13: { start: '19:20', end: '20:10' }
+  }
+  return periodTime[period]
+}
 export const fillColor = (data: Subject[]): Subject[] => {
-  let colorIndex = 0;
-  const colorsMap = new Map<string, number>();
+  let colorIndex = 0
+  const colorsMap = new Map<string, number>()
 
   for (const item of data) {
     if (item.color === undefined) {
-      const key = item.subjectCode;
-      const existingColor = colorsMap.get(key);
-      const color = existingColor !== undefined ? existingColor : colorIndex++;
-      item.color = color;
-      colorsMap.set(key, color);
+      const key = item.subjectCode
+      const existingColor = colorsMap.get(key)
+      const color = existingColor !== undefined ? existingColor : colorIndex++
+      item.color = color
+      colorsMap.set(key, color)
     }
   }
 
-  data.sort((a, b) => a.color! - b.color!);
+  data.sort((a, b) => a.color! - b.color!)
 
-  return data;
-};
+  return data
+}
