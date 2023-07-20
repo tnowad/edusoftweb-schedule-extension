1a48aefe5fe149f5c0d102f713a950e5b1eadb62
Nguyen Minh Tuan <tnowad@gmail.com>
2023-07-21 06:33:10 +0700

    refactor(files): remove all dont use

--- src/logic/storage.ts
+++ src/logic/storage.ts
@@ -1,6 +1,4 @@
 import { useStorageLocal } from '@/composables/useStorageLocal'
 import type Subject from '@/interfaces/Subject'
 
-export const storageDemo = useStorageLocal('webext-demo', 'Storage Demo')
-
 export const storageSchedule = useStorageLocal<Subject[]>('subject', [])
