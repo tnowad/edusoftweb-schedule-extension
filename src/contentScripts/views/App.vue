52283ca51a787098a10c12a976b63f59b524ccf4
Nguyen Minh Tuan <tnowad@gmail.com>
2023-07-21 03:42:21 +0700

    feat(contentScripts): add popup to show when successful save data

--- src/contentScripts/views/App.vue
+++ src/contentScripts/views/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
+import '@/styles'
 import { useToggle } from '@vueuse/core'
+import logo from '@/assets/icon.svg'
 import { assignColorsToSubjects, parseSubjectData } from '@/utils/subjectUtils'
 import type Subject from '@/interfaces/Subject'
 
-import 'uno.css'
+import { storageSchedule } from '@/logic'
 
 const [show, toggle] = useToggle(false)
 
@@ -16,26 +18,63 @@ const getSubjects = (): Subject[] => {
   return assignColorsToSubjects(data)
 }
 
-// eslint-disable-next-line no-console
-console.log(getSubjects())
+const handleSaveSubject = () => {
+  storageSchedule.value = getSubjects()
+}
 </script>
 
 <template>
-  <div class="fixed right-0 bottom-0 m-5 z-100 flex items-end font-sans select-none leading-1em">
-    <div
-      class="bg-white text-gray-800 rounded-lg shadow w-max h-min" p="x-4 y-2" m="y-auto r-2"
-      transition="opacity duration-300" :class="show ? 'opacity-100' : 'opacity-0'"
-    >
-      <h1 class="text-lg">
-        Vitesse WebExt
-      </h1>
-      <SharedSubtitle />
+  <div
+    v-if="show"
+    class="fixed right-0 bottom-0 m-10 z-100 flex items-end font-sans select-none leading-1em animate-light-speed-in-left animate-repeat-1 animate-duration-150"
+  >
+    <div class="bg-white text-gray-800 rounded-lg shadow w-max h-min shadow-xl" p="x-4 y-2" m="y-auto r-2">
+      <table class="table-auto border border-collapse">
+        <caption class="text-xl font-bold p-2">
+          Successfully Saved Subjects
+        </caption>
+        <thead aria-describedby="subject-table">
+          <tr class="bg-blue-200">
+            <th class="px-4 py-2">
+              Subject Code
+            </th>
+            <th class="px-4 py-2">
+              Subject Name
+            </th>
+            <th class="px-4 py-2">
+              Subject Group
+            </th>
+            <th class="px-4 py-2">
+              Practice Session
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="subject in storageSchedule" :key="subject.subjectCode" class="hover:bg-gray-100">
+            <td class="border px-4 py-2">
+              {{ subject.subjectCode }}
+            </td>
+            <td class="border px-4 py-2">
+              {{ subject.subjectName }}
+            </td>
+            <td class="border px-4 py-2">
+              {{ subject.subjectGroup }}
+            </td>
+            <td class="border px-4 py-2">
+              {{ subject.practiceSession }}
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </div>
-    <button
-      class="flex w-10 h-10 rounded-full shadow cursor-pointer border-none" bg="teal-600 hover:teal-700"
-      @click="toggle()"
-    >
-      <pixelarticons-power class="block m-auto text-white text-lg" />
-    </button>
   </div>
+  <button
+    class="fixed bottom-0 right-0 box-border m-5 flex w-10 h-10 rounded-xl cursor-pointer border-none items-center justify-center overflow-hidden"
+    @click="(() => {
+      handleSaveSubject();
+      toggle();
+    })"
+  >
+    <img class="h-10 w-10" :src="logo" alt="logo">
+  </button>
 </template>
--- src/logic/storage.ts
+++ src/logic/storage.ts
@@ -1,3 +1,6 @@
 import { useStorageLocal } from '@/composables/useStorageLocal'
+import type Subject from '@/interfaces/Subject'
 
 export const storageDemo = useStorageLocal('webext-demo', 'Storage Demo')
+
+export const storageSchedule = useStorageLocal<Subject[]>('subject', [])
