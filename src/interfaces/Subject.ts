f117963af0d3e1698616f18f9e29ae8671ff7a51
Nguyen Minh Tuan <tnowad@gmail.com>
2023-07-21 02:10:42 +0700

    feat(interfaces): add Subject interface

---
+++ src/interfaces/Subject.ts
@@ -0,0 +1,20 @@
+export default interface Subject {
+  subjectCode: string
+  subjectName: string
+  subjectGroup: string
+  numberOfCredits: string
+  classCode: string | null
+  creditsForTuitionFee: string
+  notAllowedToRegister: string | null
+  practiceSession: string | null
+  dayOfWeek: number
+  startingPeriod: number
+  numberOfPeriods: number
+  room: string
+  lecturer: string
+  weeks: {
+    start: number
+    end: number
+  }
+  color?: number
+}
