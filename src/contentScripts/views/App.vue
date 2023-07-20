745cd90cfd57c5dfe151ab07a6c9b7eb055bf8a8
Nguyen Minh Tuan <tnowad@gmail.com>
2023-07-21 02:17:31 +0700

    feat(contentScripts): add getSubjects to collect subject data

--- src/contentScripts/views/App.vue
+++ src/contentScripts/views/App.vue
@@ -1,18 +1,30 @@
 <script setup lang="ts">
 import { useToggle } from '@vueuse/core'
+import { assignColorsToSubjects, parseSubjectData } from '@/utils/subjectUtils'
+import type Subject from '@/interfaces/Subject'
+
 import 'uno.css'
 
 const [show, toggle] = useToggle(false)
+
+const getSubjects = (): Subject[] => {
+  const trElements = Array.from(document.querySelectorAll('.body-table tr'))
+  const data = trElements.map(trElement =>
+    parseSubjectData(Array.from(trElement.querySelectorAll('td'))),
+  )
+
+  return assignColorsToSubjects(data)
+}
+
+// eslint-disable-next-line no-console
+console.log(getSubjects())
 </script>
 
 <template>
   <div class="fixed right-0 bottom-0 m-5 z-100 flex items-end font-sans select-none leading-1em">
     <div
-      class="bg-white text-gray-800 rounded-lg shadow w-max h-min"
-      p="x-4 y-2"
-      m="y-auto r-2"
-      transition="opacity duration-300"
-      :class="show ? 'opacity-100' : 'opacity-0'"
+      class="bg-white text-gray-800 rounded-lg shadow w-max h-min" p="x-4 y-2" m="y-auto r-2"
+      transition="opacity duration-300" :class="show ? 'opacity-100' : 'opacity-0'"
     >
       <h1 class="text-lg">
         Vitesse WebExt
@@ -20,8 +32,7 @@ const [show, toggle] = useToggle(false)
       <SharedSubtitle />
     </div>
     <button
-      class="flex w-10 h-10 rounded-full shadow cursor-pointer border-none"
-      bg="teal-600 hover:teal-700"
+      class="flex w-10 h-10 rounded-full shadow cursor-pointer border-none" bg="teal-600 hover:teal-700"
       @click="toggle()"
     >
       <pixelarticons-power class="block m-auto text-white text-lg" />
