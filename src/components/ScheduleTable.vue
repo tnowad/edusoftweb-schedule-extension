a76fd521bee94c15898609f62653d96c14e0901d
Nguyen Minh Tuan <tnowad@gmail.com>
2023-07-21 06:07:17 +0700

    feat(components): add schedule table

---
+++ src/components/ScheduleTable.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+import { Icon } from '@iconify/vue'
+import SubjectTable from './SubjectTable.vue'
+import { storageSchedule } from '@/logic'
+import { formatDate } from '@/utils/dateUtils'
+import { filterSubjectsByDay } from '@/utils/subjectUtils'
+
+const subjects = ref(filterSubjectsByDay(storageSchedule.value, new Date()))
+const dateShowing = ref(formatDate(new Date()))
+
+function goToPreviousWeek() {
+  const currentDate = new Date(dateShowing.value)
+  currentDate.setDate(currentDate.getDate() - 7)
+  dateShowing.value = formatDate(currentDate)
+}
+
+function goToNextWeek() {
+  const currentDate = new Date(dateShowing.value)
+  currentDate.setDate(currentDate.getDate() + 7)
+  dateShowing.value = formatDate(currentDate)
+}
+
+watch([dateShowing], () => {
+  subjects.value = filterSubjectsByDay(storageSchedule.value, new Date(dateShowing.value))
+})
+</script>
+
+<template>
+  <div class="flex items-center justify-center w-full h-[100vh] flex-col bg-slate-50">
+    <div class="flex items-center justify-center mb-8">
+      <button
+        className="flex items-center justify-center w-8 h-8 mr-4 text-white bg-[#5cbfdd] rounded-full duration-300 hover:shadow-lg hover:scale-105"
+        @click="goToPreviousWeek"
+      >
+        <Icon icon="mdi:less-than" class="text-2xl" />
+      </button>
+      <input v-model="dateShowing" type="date" class="bg-transparent outline-none mx-3">
+      <button
+        className="flex items-center justify-center w-8 h-8 mr-4 text-white bg-[#5cbfdd] rounded-full duration-300 hover:shadow-lg hover:scale-105"
+        @click="goToNextWeek"
+      >
+        <Icon icon="mdi:greater-than" class="text-2xl" />
+      </button>
+    </div>
+    <SubjectTable />
+  </div>
+</template>
---
+++ src/components/SubjectTable.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="flex flex-col items-center justify-center w-11/12 min-w-[1200px] max-w-[1400px] h-[80vh] box-border p-2 rounded-xl shadow-md">
+    <div class="h-[80vh] grid overflow-hidden h-80vh gap-x-[1px] gap-y-[1px] w-full grid-cols-[5%_repeat(6,_1fr)_5%] grid-rows-[repeat(15,_1fr)]">
+      d
+    </div>
+  </div>
+</template>
