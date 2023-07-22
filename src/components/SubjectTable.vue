d66f4b4a519cdaaf7db376ee81958b7fe245b907
Nguyen Minh Tuan <tnowad@gmail.com>
2023-07-22 23:25:38 +0700

    feat(components): remove min-w of SubjectTable

--- src/components/SubjectTable.vue
+++ src/components/SubjectTable.vue
@@ -5,7 +5,7 @@ defineProps<{ subjects: Subject[] }>()
 </script>
 
 <template>
-  <div class="flex flex-col items-center justify-center w-11/12 min-w-[1200px] max-w-[1400px] h-[80vh] box-border p-2 rounded-xl shadow-md">
+  <div class="flex flex-col items-center justify-center w-11/12 max-w-[1400px] h-[80vh] box-border p-2 rounded-xl shadow-md">
     <div class="h-[80vh] grid overflow-hidden h-80vh gap-x-[1px] gap-y-[1px] w-full grid-cols-[5%_repeat(6,_1fr)_5%] grid-rows-[repeat(15,_1fr)]">
       <div
         v-for="index in 6"
